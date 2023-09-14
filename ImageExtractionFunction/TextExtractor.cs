5f37a04300bf706e60be270c1a04f22d1aa7bb0a
Karoline Klever <karolikl@gmail.com>
2023-09-14 12:59:33 +0200

    Added call to openAI

--- ImageExtractionFunction/TextExtractor.cs
+++ ImageExtractionFunction/TextExtractor.cs
@@ -22,9 +22,13 @@ using System;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
+using System.Net.Http;
+using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Net.Http.Json;
+using System.Collections.Generic;
 
 namespace ImageFunctions
 {
@@ -34,6 +38,9 @@ namespace ImageFunctions
         private static readonly string VISION_KEY = Environment.GetEnvironmentVariable("VISION_KEY");
         private static readonly string VISION_ENDPOINT = Environment.GetEnvironmentVariable("VISION_ENDPOINT");
         private static readonly string EXTRACTED_TEXT_CONTAINER_NAME = Environment.GetEnvironmentVariable("EXTRACTEDTEXT_CONTAINER_NAME");
+        private static readonly string OPENAI_KEY = Environment.GetEnvironmentVariable("OPENAI_KEY");
+        private static readonly string OPENAI_ENDPOINT = Environment.GetEnvironmentVariable("OPENAI_ENDPOINT");
+        private static readonly string TRANSLATEDTEXT_CONTAINER_NAME = Environment.GetEnvironmentVariable("TRANSLATEDTEXT_CONTAINER_NAME");
 
         private static string GetBlobNameFromUrl(string bloblUrl, ILogger log)
         {
@@ -127,12 +134,60 @@ namespace ImageFunctions
                     var extractedBlobName = string.Concat(blobName, ".json");
                     await blobContainerClient.UploadBlobAsync(extractedBlobName, output);
                 }
+
+                // CALL LLM
+                // Prepare the data to be sent to the API
+                var requestData = new
+                {
+                    prompt = $"In the french text below, fix all spelling mistakes and fill in any missing words. Output the results in french and english. Text: {stringBuilder.ToString()}",
+                    max_tokens = 200 // Adjust as needed
+                };
+
+                using (var client = new HttpClient())
+                {
+                    // Set the headers with your API key
+                    client.DefaultRequestHeaders.Add("Authorization", $"Bearer {OPENAI_KEY}");
+
+                    log.LogInformation($"Calling OpenAI.");
+                    // Make the API call to OpenAI
+                    var response = await client.PostAsJsonAsync(OPENAI_ENDPOINT, requestData);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        log.LogInformation($"OpenAI response successful");
+                        var apiResponse = await response.Content.ReadFromJsonAsync<ApiResponse>();
+                        var openAIoutput = apiResponse.choices[0].text;
+
+                        var finalBlobContainerClient = blobServiceClient.GetBlobContainerClient(TRANSLATEDTEXT_CONTAINER_NAME);
+                        using (var output = new MemoryStream())
+                        {
+                            byte[] bytes = Encoding.UTF8.GetBytes(openAIoutput);
+                            output.Write(bytes, 0, bytes.Length);
+                            output.Position = 0;
+                            var translatedBlobName = string.Concat(blobName, "_tranlation.json");
+                            await finalBlobContainerClient.UploadBlobAsync(translatedBlobName, output);
+                        }
+                    }
+                    else
+                    {
+                        log.LogError($"Failed to process the text. Status Code: {response.StatusCode}");
+                    }
+                }
             }
             catch (Exception ex)
             {
                 log.LogError(ex.Message);
                 throw;
             }
+        }
+    }
+    class ApiResponse
+    {
+        public List<Choice> choices { get; set; }
+
+        public class Choice
+        {
+            public string text { get; set; }
         }
     }
 }
