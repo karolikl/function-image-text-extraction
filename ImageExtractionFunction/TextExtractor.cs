7353d4da01af692a7d12a40adf912e42d14e3d4f
Karoline Klever <karolikl@gmail.com>
2023-09-14 09:48:43 +0200

    Refactored function

--- ImageExtractionFunction/TextExtractor.cs
+++ ImageExtractionFunction/TextExtractor.cs
@@ -15,9 +15,12 @@ using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Extensions.Logging;
+using Microsoft.VisualBasic;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.IO;
+using System.IO.Compression;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -29,6 +32,7 @@ namespace ImageFunctions
         private static readonly string BLOB_STORAGE_CONNECTION_STRING = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
         private static readonly string VISION_KEY = Environment.GetEnvironmentVariable("VISION_KEY");
         private static readonly string VISION_ENDPOINT = Environment.GetEnvironmentVariable("VISION_ENDPOINT");
+        private static readonly string EXTRACTED_TEXT_CONTAINER_NAME = Environment.GetEnvironmentVariable("EXTRACTEDTEXT_CONTAINER_NAME");
 
         private static string GetBlobNameFromUrl(string bloblUrl)
         {
@@ -36,57 +40,6 @@ namespace ImageFunctions
             var blobClient = new BlobClient(uri);
             return blobClient.Name;
         }
-
-        private static async Task ExtractTextFromImage(string bloblUrl,
-            ILogger log)
-        {
-            try
-            {
-                ComputerVisionClient client = AuthenticateVision(VISION_ENDPOINT, VISION_KEY);
-                if (client == null)
-                    log.LogError("Could not initialize ComputerVisionClient, authentication failed");
-
-                // Read text from URL
-                var textHeaders = await client.ReadAsync(bloblUrl);
-                // After the request, get the operation location (operation ID)
-                string operationLocation = textHeaders.OperationLocation;
-                Thread.Sleep(2000);
-
-                // Retrieve the URI where the extracted text will be stored from the Operation-Location header.
-                // We only need the ID and not the full URL
-                const int numberOfCharsInOperationId = 36;
-                string operationId = operationLocation.Substring(operationLocation.Length - numberOfCharsInOperationId);
-
-                // Extract the text
-                ReadOperationResult results;
-                log.LogInformation($"Extracting text from URL file {Path.GetFileName(bloblUrl)}...");
-
-                do
-                {
-                    results = await client.GetReadResultAsync(Guid.Parse(operationId));
-                }
-                while ((results.Status == OperationStatusCodes.Running ||
-                    results.Status == OperationStatusCodes.NotStarted));
-
-                // Display the found text.
-                var textUrlFileResults = results.AnalyzeResult.ReadResults;
-                var stringBuilder = new StringBuilder("");
-                foreach (ReadResult page in textUrlFileResults)
-                {
-                    foreach (Line line in page.Lines)
-                    {
-                        stringBuilder.AppendLine(line.Text);
-                        log.LogInformation(line.Text);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                log.LogInformation(ex.Message);
-                throw;
-            }
-        }
-
         public static ComputerVisionClient AuthenticateVision(string endpoint, string key)
         {
             ComputerVisionClient client =
@@ -97,35 +50,30 @@ namespace ImageFunctions
 
         [FunctionName("TextExtractor")]
         public static async Task Run(
-            [EventGridTrigger] EventGridEvent eventGridEvent,
-            [Blob("{data.url}", FileAccess.Read)] Stream input,
-            ILogger log)
+        [EventGridTrigger] EventGridEvent eventGridEvent,
+        [Blob("{data.url}", FileAccess.Read)] Stream blobStream,
+        ILogger log)
         {
             try
             {
-                if (input != null)
+                // Deserialize the Event Grid event data
+                var eventData = JsonConvert.DeserializeObject<StorageBlobCreatedEventData>(eventGridEvent.Data.ToString());
+
+                // Process the Blob data from the Blob Storage trigger
+                if (blobStream != null)
                 {
-                    var dataAsJObject = JObject.Parse(eventGridEvent.Data.ToString());
-                    var createdEvent = dataAsJObject.ToObject<StorageBlobCreatedEventData>();
+                    var imageUrl = eventData.Url;
+                    log.LogInformation($"Received Blob: {imageUrl}");
 
-                    var imageUrl = createdEvent.Url;
-                    var extension = Path.GetExtension(createdEvent.Url);
+                    var blobName = GetBlobNameFromUrl(imageUrl);
+                    log.LogInformation($"Extracted blob name from url: {blobName}");
 
-
-                    var thumbContainerName = Environment.GetEnvironmentVariable("EXTRACTEDTEXT_CONTAINER_NAME");
-                    var blobServiceClient = new BlobServiceClient(BLOB_STORAGE_CONNECTION_STRING);
-                    var blobContainerClient = blobServiceClient.GetBlobContainerClient(thumbContainerName);
-                    var blobName = GetBlobNameFromUrl(createdEvent.Url);
-
-
-                    ComputerVisionClient client = AuthenticateVision(VISION_ENDPOINT, VISION_KEY);
-                    if (client == null)
+                    ComputerVisionClient visionClient = AuthenticateVision(VISION_ENDPOINT, VISION_KEY);
+                    if (visionClient == null)
                         log.LogError("Could not initialize ComputerVisionClient, authentication failed");
 
-                    // Read text from URL
-                    var textHeaders = await client.ReadAsync(imageUrl);
-                    // After the request, get the operation location (operation ID)
-                    string operationLocation = textHeaders.OperationLocation;
+                    var textHeaders = await visionClient.ReadAsync(imageUrl);  // Read text from URL
+                    string operationLocation = textHeaders.OperationLocation; // After the request, get the operation location (operation ID)
                     Thread.Sleep(2000);
 
                     // Retrieve the URI where the extracted text will be stored from the Operation-Location header.
@@ -135,11 +83,12 @@ namespace ImageFunctions
 
                     // Extract the text
                     ReadOperationResult results;
-                    log.LogInformation($"Extracting text from URL file {Path.GetFileName(imageUrl)}...");
+                    log.LogInformation($"Extracting text from URL file {blobName}...");
 
                     do
                     {
-                        results = await client.GetReadResultAsync(Guid.Parse(operationId));
+                        results = await visionClient.GetReadResultAsync(Guid.Parse(operationId));
+                        log.LogInformation($"Waiting to read the results.... {results.Status}.");
                     }
                     while ((results.Status == OperationStatusCodes.Running ||
                         results.Status == OperationStatusCodes.NotStarted));
@@ -156,6 +105,8 @@ namespace ImageFunctions
                         }
                     }
 
+                    var blobServiceClient = new BlobServiceClient(BLOB_STORAGE_CONNECTION_STRING);
+                    var blobContainerClient = blobServiceClient.GetBlobContainerClient(EXTRACTED_TEXT_CONTAINER_NAME);
                     using (var output = new MemoryStream())
                     {
                         byte[] bytes = Encoding.UTF8.GetBytes(stringBuilder.ToString());
@@ -167,7 +118,7 @@ namespace ImageFunctions
             }
             catch (Exception ex)
             {
-                log.LogInformation(ex.Message);
+                log.LogError(ex.Message);
                 throw;
             }
         }
