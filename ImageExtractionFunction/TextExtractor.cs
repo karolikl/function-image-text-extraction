519d70d38d860e659e29fc241b34532988ea0527
Karoline Klever <karolikl@gmail.com>
2023-09-14 10:08:05 +0200

    Adding null reference check for eventData object

--- ImageExtractionFunction/TextExtractor.cs
+++ ImageExtractionFunction/TextExtractor.cs
@@ -60,62 +60,60 @@ namespace ImageFunctions
 
                 // Deserialize the Event Grid event data
                 var eventData = JsonConvert.DeserializeObject<StorageBlobCreatedEventData>(eventGridEvent.Data.ToString());
+                if (eventData == null)
+                    log.LogError("Could not deserialize eventGridEvent to StorageBlobCreatedEventData");
 
-                // Process the Blob data from the Blob Storage trigger
-                if (blobStream != null)
+                var imageUrl = eventData.Url;
+                log.LogInformation($"Received Blob: {imageUrl}");
+
+                var blobName = GetBlobNameFromUrl(imageUrl);
+                log.LogInformation($"Extracted blob name from url: {blobName}");
+
+                ComputerVisionClient visionClient = AuthenticateVision(VISION_ENDPOINT, VISION_KEY);
+                if (visionClient == null)
+                    log.LogError("Could not initialize ComputerVisionClient, authentication failed");
+
+                var textHeaders = await visionClient.ReadAsync(imageUrl);  // Read text from URL
+                string operationLocation = textHeaders.OperationLocation; // After the request, get the operation location (operation ID)
+                Thread.Sleep(2000);
+
+                // Retrieve the URI where the extracted text will be stored from the Operation-Location header.
+                // We only need the ID and not the full URL
+                const int numberOfCharsInOperationId = 36;
+                string operationId = operationLocation.Substring(operationLocation.Length - numberOfCharsInOperationId);
+
+                // Extract the text
+                ReadOperationResult results;
+                log.LogInformation($"Extracting text from URL file {blobName}...");
+
+                do
                 {
-                    var imageUrl = eventData.Url;
-                    log.LogInformation($"Received Blob: {imageUrl}");
+                    results = await visionClient.GetReadResultAsync(Guid.Parse(operationId));
+                    log.LogInformation($"Waiting to read the results.... {results.Status}.");
+                }
+                while ((results.Status == OperationStatusCodes.Running ||
+                    results.Status == OperationStatusCodes.NotStarted));
 
-                    var blobName = GetBlobNameFromUrl(imageUrl);
-                    log.LogInformation($"Extracted blob name from url: {blobName}");
-
-                    ComputerVisionClient visionClient = AuthenticateVision(VISION_ENDPOINT, VISION_KEY);
-                    if (visionClient == null)
-                        log.LogError("Could not initialize ComputerVisionClient, authentication failed");
-
-                    var textHeaders = await visionClient.ReadAsync(imageUrl);  // Read text from URL
-                    string operationLocation = textHeaders.OperationLocation; // After the request, get the operation location (operation ID)
-                    Thread.Sleep(2000);
-
-                    // Retrieve the URI where the extracted text will be stored from the Operation-Location header.
-                    // We only need the ID and not the full URL
-                    const int numberOfCharsInOperationId = 36;
-                    string operationId = operationLocation.Substring(operationLocation.Length - numberOfCharsInOperationId);
-
-                    // Extract the text
-                    ReadOperationResult results;
-                    log.LogInformation($"Extracting text from URL file {blobName}...");
-
-                    do
+                // Display the found text.
+                var textUrlFileResults = results.AnalyzeResult.ReadResults;
+                var stringBuilder = new StringBuilder("");
+                foreach (ReadResult page in textUrlFileResults)
+                {
+                    foreach (Line line in page.Lines)
                     {
-                        results = await visionClient.GetReadResultAsync(Guid.Parse(operationId));
-                        log.LogInformation($"Waiting to read the results.... {results.Status}.");
+                        stringBuilder.AppendLine(line.Text);
+                        log.LogInformation(line.Text);
                     }
-                    while ((results.Status == OperationStatusCodes.Running ||
-                        results.Status == OperationStatusCodes.NotStarted));
+                }
 
-                    // Display the found text.
-                    var textUrlFileResults = results.AnalyzeResult.ReadResults;
-                    var stringBuilder = new StringBuilder("");
-                    foreach (ReadResult page in textUrlFileResults)
-                    {
-                        foreach (Line line in page.Lines)
-                        {
-                            stringBuilder.AppendLine(line.Text);
-                            log.LogInformation(line.Text);
-                        }
-                    }
-
-                    var blobServiceClient = new BlobServiceClient(BLOB_STORAGE_CONNECTION_STRING);
-                    var blobContainerClient = blobServiceClient.GetBlobContainerClient(EXTRACTED_TEXT_CONTAINER_NAME);
-                    using (var output = new MemoryStream())
-                    {
-                        byte[] bytes = Encoding.UTF8.GetBytes(stringBuilder.ToString());
-                        output.Write(bytes, 0, bytes.Length);
-                        output.Position = 0;
-                        await blobContainerClient.UploadBlobAsync(blobName, output);
-                    }
+                var blobServiceClient = new BlobServiceClient(BLOB_STORAGE_CONNECTION_STRING);
+                var blobContainerClient = blobServiceClient.GetBlobContainerClient(EXTRACTED_TEXT_CONTAINER_NAME);
+                using (var output = new MemoryStream())
+                {
+                    byte[] bytes = Encoding.UTF8.GetBytes(stringBuilder.ToString());
+                    output.Write(bytes, 0, bytes.Length);
+                    output.Position = 0;
+                    await blobContainerClient.UploadBlobAsync(blobName, output);
                 }
             }
             catch (Exception ex)
